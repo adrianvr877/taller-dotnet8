5e01694356fcb02709df7d74d507e4df301f79ef
Renato Alves Felix <renatofelix.redhat@gmail.com>
2026-03-10 16:27:36 -0300

    olax

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => Results.Json("hello"));
+app.MapGet("/", () => Results.Json("ola"));
 
 app.Run();
