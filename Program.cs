50fc346a447f7b302efcd3d7e6e4c2ca932ba63d
Renato Alves Felix <renatofelix.redhat@gmail.com>
2026-03-10 16:17:18 -0300

    world

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => Results.Json("hello"));
+app.MapGet("/", () => Results.Json("world"));
 
 app.Run();
