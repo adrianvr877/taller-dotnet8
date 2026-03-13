26a4f4dded4457ebf125c815bbf6c4356a75c24d
adrianvr877 <87092595+adrianvr877@users.noreply.github.com>
2026-03-13 12:46:50 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/weatherforecast", () => Results.Json("docker webhook secret"));
+app.MapGet("/weatherforecast", () => Results.Json("docker webhook"));
 
 app.Run();
 // var builder = WebApplication.CreateBuilder(args);
